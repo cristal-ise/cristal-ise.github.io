24788430f214590e0e28e59cf4a5bff29cefda61
Zsolt Kovcs <zs.myth@gmail.com>
2014-08-28 21:15:53 +0200

    Countdown was added to the header

---
+++ assets/js/teplok-coming-soon.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	/** COUNTDOWN SELECTOR **/
+	$("#header-landing").countdown({
+		date: "15 October 2014 09:00:00", /** Change this to your desired date to countdown to **/
+		format: "off"
+	});
+});
